8c99430140734fdaaad1637ccadbd4a9db272443
James Yarrington <jyarrington@vt.edu>
2018-03-15 11:12:52 -0400

    added counting class.  All endpoints GET

--- Locust/NETS_usecase_load.py
+++ Locust/NETS_usecase_load.py
@@ -27,20 +27,48 @@ class UserBehavior(TaskSet):
 
 	@task(1)
 	def structured(self):
-		self.client.post("/search/structured")
-		# Probably a POST?
+		self.client.get("/search/structured?%s" % next_item_in_cycle(EV.structured_parameters))
 
+	def on_start(self):
+		LocustCounter.increment()
+
+
+def on_request_completion(request_type, name, response_time, response_length = 0, exception = ""):
+	results = [LocustCounter.get(), request_type, name, response_time, response_length, exception]
+
+	# Append the data to the results file.
+	with open("results.csv", "a") as results_file:
+		writer = csv.writer(results_file)
+		writer.writerow(results)
 
 
 # Required for running a locust test.  Defines the user properites.
 class WebsiteUser(HttpLocust):
-    task_set = UserBehavior
+	task_set = UserBehavior
 
-    # No need to send too many status requests.  Send one every 2 seconds.
-    min_wait = 2000
-    max_wait = 2000
+	min_wait = 400
+	max_wait = 400
+
+	events.request_success += on_request_completion
+	events.request_failure += on_request_completion
+
+	# Write the header of the results file.
+	with open("results.csv", "w+") as results_file:
+		writer = csv.writer(results_file)
+		writer.writerow(["Users", "Request Type", "Name", "Response Time", "Length", "Exception"])
 
 def next_item_in_cycle(l):
 	item = l.pop()
 	l.insert(0, item)
 	return item
+
+class LocustCounter:
+	count = 0
+
+	@staticmethod
+	def increment():
+		LocustCounter.count += 1
+
+	@staticmethod
+	def get():
+		return LocustCounter.count
